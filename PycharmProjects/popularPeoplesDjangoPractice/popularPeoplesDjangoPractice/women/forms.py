1d9c71e3ee4fd66b92017de61f35044c50c1efa0
yxane <yxanee@gmail.com>
2022-10-06 04:45:54 +0300

    added registration

--- PycharmProjects/popularPeoplesDjangoPractice/popularPeoplesDjangoPractice/women/forms.py
+++ PycharmProjects/popularPeoplesDjangoPractice/popularPeoplesDjangoPractice/women/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
-
+from django.contrib.auth.models import User
 from .models import *
 
 
@@ -22,8 +23,20 @@ class AddPostForm(forms.ModelForm):
         if len(title) > 200:
             raise ValidationError('Длинна превышает 200 символов')
         return title
+
     # title = forms.CharField(max_length=255, label='Заголовок')
     # slug = forms.SlugField(max_length=255, label='Слаг')
     # content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Контент')
     # is_published = forms.BooleanField(label='Опубликовано', required=False)
     # cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Категория не выбрана')
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
